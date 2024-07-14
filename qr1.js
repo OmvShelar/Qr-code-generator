3afacd6e9290ad24c66d03fb5e9d1dae6efd9a55
OmvShelar <omshelar09@gmail.com>
2024-07-14 20:33:27 +0530

    file changed

--- qr1.js
+++ qr1.js
@@ -6,24 +6,24 @@ const QRCode = require('qrcode');
 
 const generateQrCode = async (text) => {
     try {
-      const qrCodeData = await QRCode.toFile(path.join(__dirname,'Qr_codes',`${text}.png`) ,text);
-      console.log('QR Code generated successfully!');
+        const qrCodeData = await QRCode.toFile(path.join(__dirname, 'Qr_codes', `${text}.png`), text);
+        console.log('QR Code generated successfully!');
     } catch (err) {
-      console.error('Failed to generate QR Code:', err);
+        console.error('Failed to generate QR Code:', err);
     }
-  };
+};
 
-  inquirer.prompt([
+inquirer.prompt([
     {
-      type: 'input',
-      name: 'text',
-      message: 'Enter the text to generate QR code:',
+        type: 'input',
+        name: 'text',
+        message: 'Enter the text to generate QR code:',
     },
-  ])
-  .then((answers) => {
-    generateQrCode(answers.text);
-  })
-  
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+])
+    .then((answers) => {
+        generateQrCode(answers.text);
+    })
+
+    .catch((error) => {
+        console.error('Error:', error);
+    });
